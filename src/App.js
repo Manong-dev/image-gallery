c57ca134b7bf4da5aa535edebf76e715914e1de8
Luis <ninsa@tutanota.com>
2024-03-24 19:39:38 +0900

    deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Gallery from "./components/Gallery";
+import GalleryProps from "./components/GalleryProps";
 
-function App() {
+// Galllery
+import img_1 from "./assets/summer-palace.jpg";
+import img_2 from "./assets/shanghai-city.jpg";
+import img_3 from "./assets/warriors.jpg";
+import img_4 from "./assets/greatwall-china.jpg";
+
+const IMAGES_GALLERY = [
+  { url1: img_1, alt1: "Summer Palace" },
+  { url2: img_2, alt2: "Shanghai" },
+  { url3: img_3, alt3: "Terracotta warriors"},
+  { url4: img_4, alt4: "Chinese Greatwall"}
+];
+
+const App = () => {
+  const cardTitle1 = "Summer Palace"
+  const CardSubtitle1 = "Summer Palace"
+  const page1 = "/"
+
+  const cardTitle2 = "Shanghai"
+  const CardSubtitle2 = "Shanghai"
+  const page2 = "/"
+
+  const cardTitle3 = "Warriors"
+  const CardSubtitle3 = "Warriors"
+  const page3 = "/" 
+
+
+  const cardTitle4 = "Greatwall"
+  const CardSubtitle4 = "Greatwall"
+  const page4 = "/"
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Gallery />
+      <GalleryProps images={IMAGES_GALLERY} page1={page1} title1={cardTitle1} subtitle1={CardSubtitle1} page2={page2} title2={cardTitle2} subtitle2={CardSubtitle2} page3={page3} title3={cardTitle3} subtitle3={CardSubtitle3} page4={page4} title4={cardTitle4} subtitle4={CardSubtitle4} />
+     
     </div>
   );
 }
